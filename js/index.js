ecf0519c0e1fdc32a6c69ac193c8b321adff97f2
DavidMontanoDev <davidmotano1997developerweb@gmail.com>
2023-09-09 06:05:26 -0500

    Creando estructura html de la ventan modal de facturacion

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 import Router from "./modules/router.js";
 
+// helpers
+import ShowModal from "./../helpers/Show_modal_window.js";
+
 document.addEventListener("DOMContentLoaded", Main);
 
 function Main(){
@@ -7,7 +10,13 @@ function Main(){
     const ROUTER = new Router();
     ROUTER.startRouter();
 
+    // helpers = Funcion de ayuda renderiza la ventana modal de la factura.
+    ShowModal(ROUTER);
+
     window.addEventListener("hashchange", (e) => {
         ROUTER.startRouter();
+        
+        // helpers = Funcion de ayuda renderiza la ventana modal de la factura.
+        ShowModal(ROUTER);
     });
 };

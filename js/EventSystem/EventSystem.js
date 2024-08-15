28254234f08c9c632e08b706571c7630d7d1a589
DavidMontanoDev <davidmotano1997developerweb@gmail.com>
2024-08-15 12:22:31 -0500

    Implementando la clase del sistema de eventos y adaptandola al patron singleton

---
+++ js/EventSystem/EventSystem.js
@@ -0,0 +1,23 @@
+class EventSystem {
+    static #instance;
+
+    constructor(){
+        if(EventSystem.#instance){
+            // Evitamos la creación de más intancias ya que es importante mantener un estado global de este modulo. 
+            // Para ello utlizamos el patron singletón.
+            throw Error("El sistema de eventos ya ha sido instanciado. Usa 'EventSystem.instance' para acceder a la instancia.");
+        };
+
+        EventSystem.#instance = this;
+    };
+
+    static get instance(){ // Propiedad estatica para tener acceso de forma global a la instancia única.
+        if(!EventSystem.#instance){
+            throw Error("El sistema de eventos no ha sido instanciado. Crea una instancia usando << new EventSystem() >>.");
+        };
+        
+        return EventSystem.#instance;
+    };
+};
+
+export default EventSystem;

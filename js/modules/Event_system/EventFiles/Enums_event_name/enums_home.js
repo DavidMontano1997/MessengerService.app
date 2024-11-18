65d24d23d373aaa1f981aad9bc6a9b396a50c397
DavidMontanoDev <davidmotano1997developerweb@gmail.com>
2024-11-18 05:38:21 -0500

    Implementando enums para la vista home, se registra un evento para que sirva de ejemplo

---
+++ js/modules/Event_system/EventFiles/Enums_event_name/enums_home.js
@@ -0,0 +1,6 @@
+const ENUMS_EVENT_NAME_HOME = Object.freeze({
+    // de prueba
+    GET_INCOME_FULL : "get_income_full"
+});
+
+export default ENUMS_EVENT_NAME_HOME;
---
+++ js/modules/Event_system/EventFiles/home.js
@@ -0,0 +1,14 @@
+import EventSystem from "../Event_system.js";
+import ENUMS_EVENT_NAME_HOME from "./Enums_event_name/enums_home.js"; // nombres de eventos.
+import Logica from "../../../LogicaPRUEBA.js"; // Archivo de prueba: Contiene toda la logica de negocio.
+
+const CATEGORY = "home"; // Definimos la categoria o vista en la cuál se van a registrar los eventos en el
+// sistema.
+
+const HOME_EVENT_STORE = EventSystem.instance; // obtenemos la única instancia al sistema de eventos.
+
+// Registro de eventos
+HOME_EVENT_STORE.registerEvent(ENUMS_EVENT_NAME_HOME.GET_INCOME_FULL,{
+    view: CATEGORY, // Vista/categoria en la cuál se va almacenar el evento
+    service: Logica.getIngresoFull // Servicio o logica de negocio que debera ejecutar una vez que la UI dispare el evento.
+});
